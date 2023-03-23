46e5f3b09bfa0fc6336c25ca76e534bc5068f341
StephenBoulter <sb3226@gmail.com>
2023-03-23 11:11:33 +1100

    tf fix

--- src/locals.tf
+++ src/locals.tf
@@ -5,13 +5,15 @@ locals {
     environment = terraform.workspace
   }
 
+  sub = substr(data.azurerm_subscription.current.subscription_id, 0, 4)
+
   name_prefix         = var.naming_prefix
   resource_group_name = lower("rg-${local.name_prefix}-${terraform.workspace}-${var.location}")
   storageAccountName  = lower("sa${local.name_prefix}${terraform.workspace}${var.location}")
   vnet                = lower("vnet-${local.name_prefix}-${terraform.workspace}-${var.location}")
-  apim_name           = lower("am-${local.name_prefix}-${terraform.workspace}-${var.location}")
+  apim_name           = lower("apim-${local.sub}-${local.name_prefix}-${terraform.workspace}-${var.location}")
   sb_name             = lower("sb-${local.name_prefix}-${terraform.workspace}-${var.location}")
-  public_ip           = lower("pip-${local.name_prefix}-${terraform.workspace}-${var.location}")
+  public_ip           = lower("pip-${local.sub}-${local.name_prefix}-${terraform.workspace}-${var.location}")
   # webapp infrustructure
   sp_name             = lower("sp${local.name_prefix}${terraform.workspace}${var.location}")
   api_name            = lower("api-${local.name_prefix}-${terraform.workspace}")
