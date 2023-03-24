5bb14419fc62bed11b633c89a004bdc4aee26185
StephenBoulter <sb3226@gmail.com>
2023-03-24 14:56:58 +1100

    deploying app services

--- src/modules/apps/main.tf
+++ src/modules/apps/main.tf
@@ -27,50 +27,49 @@ resource "azurerm_linux_web_app" "identity" {
   }
 }
 
-# data "azurerm_servicebus_namespace_authorization_rule" "authRule" {
-#   name                = "messagebus-policy"
-#   namespace_name      = var.sb_name
-#   resource_group_name = var.resource_group_name
-# }
-#
-# resource "azurerm_linux_web_app" "api" {
-#   name                = var.api_name
-#   resource_group_name = var.resource_group_name
-#   location            = azurerm_service_plan.app_sp.location
-#   service_plan_id     = azurerm_service_plan.app_sp.id
-#   depends_on = [
-#     azurerm_service_plan.app_sp,
-#   ]
-#   site_config {
-#         app_command_line = "dotnet api.dll"
-#   }
-#   app_settings = {
-#     "Authority" = "https://ltf-id-dev.azurewebsites.net"
-#     "SbConnectionString" = data.azurerm_servicebus_namespace_authorization_rule.authRule.primary_connection_string
-#     "AllowedCorsOrigins" = "https://ltf-apim-dev.azure-api.net http://localhost:4200"
-#   }
-# }
-#
-# resource "azurerm_linux_web_app" "bff" {
-#   name                = var.js_name
-#   resource_group_name = var.resource_group_name
-#   location            = azurerm_service_plan.app_sp.location
-#   service_plan_id     = azurerm_service_plan.app_sp.id
-#   depends_on = [
-#     azurerm_service_plan.app_sp,
-#   ]
-#   site_config {
-#     app_command_line = "dotnet bff.dll"
-#   }
-#   app_settings = {
-#     "Authority" = "https://ltf-id-dev.azurewebsites.net"
-#     "RemoteUri" = "https://ltf-api-dev.azurewebsites.net"
-#     "ApimUri" = "https://ltf-apim-dev.azure-api.net"
-#   }
-# }
-#
-# resource "azurerm_static_site" "ng" {
-#   name                = "ng"
-#   resource_group_name = var.resource_group_name
-#   location            = "eastus2"
-# }
+data "azurerm_servicebus_namespace_authorization_rule" "authRule" {
+  name                = "messagebus-policy"
+  namespace_name      = var.sb_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_api_management" "apim" {
+  name                = var.apim_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_linux_web_app" "api" {
+  name                = var.api_name
+  resource_group_name = var.resource_group_name
+  location            = azurerm_service_plan.app_sp.location
+  service_plan_id     = azurerm_service_plan.app_sp.id
+  depends_on = [
+    azurerm_service_plan.app_sp,
+  ]
+  site_config {
+        app_command_line = "dotnet api.dll"
+  }
+  app_settings = {
+    "Authority" = "https://${var.auth_name}.azurewebsites.net"
+    "SbConnectionString" = data.azurerm_servicebus_namespace_authorization_rule.authRule.primary_connection_string
+    "AllowedCorsOrigins" = "${data.azurerm_api_management.apim.gateway_url} http://localhost:4200"
+  }
+}
+
+resource "azurerm_linux_web_app" "bff" {
+  name                = var.js_name
+  resource_group_name = var.resource_group_name
+  location            = azurerm_service_plan.app_sp.location
+  service_plan_id     = azurerm_service_plan.app_sp.id
+  depends_on = [
+    azurerm_service_plan.app_sp,
+  ]
+  site_config {
+    app_command_line = "dotnet bff.dll"
+  }
+  app_settings = {
+    "Authority" = "https://${var.auth_name}.azurewebsites.net"
+    "RemoteUri" = "https://${var.api_name}.azurewebsites.net"
+    "ApimUri" = data.azurerm_api_management.apim.gateway_url
+  }
+}
