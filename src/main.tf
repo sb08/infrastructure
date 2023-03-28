c4915462e5887ef169f6005afaec8a16b8dc317d
StephenBoulter <sb3226@gmail.com>
2023-03-28 16:38:31 +1100

    deploy step 6

--- src/main.tf
+++ src/main.tf
@@ -79,17 +79,17 @@ module "sb" {
   depends_on          = [module.apim]
 }
 
-# module "apps" {
-#   source                = "./modules/apps"
-#   app_service_plan_name = local.sp_name
-#   auth_name             = local.auth_name
-#   api_name              = local.api_name
-#   js_name               = local.js_name
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   location              = azurerm_resource_group.rg.location
-#   common_tags           = local.common_tags
-#   sku_name              = var.sku_name
-#   sb_name               = local.sb_name
-#   apim_name             = local.apim_name
-#   depends_on            = [module.apim]
-# }
+module "apps" {
+  source                = "./modules/apps"
+  app_service_plan_name = local.sp_name
+  auth_name             = local.auth_name
+  api_name              = local.api_name
+  js_name               = local.js_name
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  common_tags           = local.common_tags
+  sku_name              = var.sku_name
+  sb_name               = local.sb_name
+  apim_name             = local.apim_name
+  depends_on            = [module.apim, module.sb]
+}
